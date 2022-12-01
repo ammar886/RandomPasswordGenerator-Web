8c82243a7fabb08ddd36206f6231d3cf5e200b5b
Ammar Khalid <81477038+ammar886@users.noreply.github.com>
2022-12-01 19:31:31 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passField_1 = document.getElementById("passField-1");
+let passField_2 = document.getElementById("passField-2");
+
+let pass1 = [];
+let pass2 = [];
+function getRandomPass(){
+    for(let i=0;i<15;i++){
+        let randomInt = Math.floor(Math.random()*92);
+        pass1[i] = characters[randomInt];   
+    }
+   
+
+    
+    for(let i=0; i<15; i++){
+        passField_1.textContent += pass1[i];
+    }
+
+    for(let i=0; i<15; i++){
+        passField_2.textContent += pass1[i];
+    }
+}
+
+function resetPass(){
+    passField_1.textContent = " ";
+    passField_2.textContent = " ";
+}
